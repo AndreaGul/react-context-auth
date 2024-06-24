19f1898b714122153123de05a5c7b86f2cec05c8
AndreaGul <andreagulino99@gmail.com>
2024-06-24 16:49:58 +0200

    logica auth context

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,15 +1,33 @@
-import { createContext , useContext } from "react";
+import { createContext , useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const AuthContext = createContext();
 
 const AuthProvider = ({children})=>{
 
-    const value={
+    const navigate = useNavigate();
 
+    const [isLoggedIn, seIsLoggedIn] = useState(false);
+
+    const login = (payload) => {
+        setIsLoggedIn(true);
+        navigate('/home');
+    }
+
+    const logout = ()=>{
+        seIsLoggedIn(false);
+        navigate('/login');
+    }
+
+
+    const value={
+        isLoggedIn,
+        login,
+        logout,
     }
     return(
         <AuthContext.Provider value={value}>
-        {children}
+            {children}
         </AuthContext.Provider>
     )
 }
@@ -18,8 +36,8 @@ const useAuth = ()=>{
     const value = useContext(AuthContext);
     if(value === undefined){
         throw new Error('Non sei dentro al Auth Provider.');
-        return value;
     }
+    return value;
 }
 
 export {AuthProvider, useAuth}
