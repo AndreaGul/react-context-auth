e33911e7014469032d898665f82d0a0fc6747c7e
AndreaGul <andreagulino99@gmail.com>
2024-06-24 17:55:28 +0200

    feat:pulsante logout

--- src/pages/Profilo.jsx
+++ src/pages/Profilo.jsx
@@ -1,7 +1,13 @@
+import { useAuth } from "../contexts/AuthContext"
+
 export default function (){
+
+    const { logout } = useAuth();
+
     return(
         <>
         <h1>Sei Loggato</h1>
+        <button onClick={logout} >logout</button>
         </>
     )
 }
